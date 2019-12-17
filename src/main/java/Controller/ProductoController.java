4a4f0dabcc7c69479422d4fefbeb3fa486d0414a
Gamzeescript <gamzeescript@outlook.com>
2019-12-17 11:44:21 -0600

    subiendo controller producto
    lo arregle porque presentaba conflicto

---
+++ src/main/java/Controller/ProductoController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named(value = "ProductoController")
+@RequestScoped
+public class ProductoController implements Serializable{
+    
+    private String mensaje;
+    
+}
