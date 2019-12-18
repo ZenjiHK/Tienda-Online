3d828bb0ab8f9149fb5dfdd7af5757533a879552
maynor009 <58366132+maynor009@users.noreply.github.com>
2019-12-18 08:34:09 -0600

    Add files via upload

---
+++ src/main/java/Controller/PaisController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import EJB.PaisFacadeLocal;
+import Entity.Pais;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author maynor.menjivarusam
+ */
+@ManagedBean
+@SessionScoped
+public class PaisController implements Serializable{
+    @EJB
+    private PaisFacadeLocal paisEJB;
+    private Pais pais;
+    private List<Pais> listaPais;
+//asdasdasdasdas
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public List<Pais> getListaPais() {
+        this.listaPais=this.paisEJB.findAll();
+        return listaPais;
+    }
+
+    public void setListaPais(List<Pais> listaPais) {
+        this.listaPais = listaPais;
+    }
+    
+    @PostConstruct
+    public void init(){
+    pais=new Pais();
+    }
+    
+    
+}
