ca92b3b8a6375934bd034e9a15a340fd1af7821b
Alex Lemuz <lemuz101@gmail.com>
2020-01-03 16:42:02 -0600

    forma pago

--- src/main/java/Controller/FormaPagoController.java
+++ src/main/java/Controller/FormaPagoController.java
@@ -13,14 +13,14 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 
 /**
  *
  * @author evelyn.andradeusam
  */
-@ManagedBean
+@Named(value = "formaPagoController")
 @SessionScoped
 public class FormaPagoController implements Serializable{
     
@@ -83,9 +83,9 @@ public class FormaPagoController implements Serializable{
         FacesContext.getCurrentInstance().addMessage(null, mensaje);
     }
     
-    public void eliminar(){
+    public void eliminar(FormaPago fp){
         try{
-            formaPagoEJB.remove(this.formapago);
+            formaPagoEJB.remove(fp);
             limpiar();
             msg = "Exito";
         }catch(Exception e){
