6da8648c05307b5f32ca5a18dbba74ae0afaf6e6
Alex Lemuz <lemuz101@gmail.com>
2020-01-07 14:17:41 -0600

    07-01-2020 Alex

--- target/generated-sources/annotations/Entity/DetalleTarjeta_.java
+++ target/generated-sources/annotations/Entity/DetalleTarjeta_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T10:51:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:08:45")
 @StaticMetamodel(DetalleTarjeta.class)
 public class DetalleTarjeta_ { 
 
