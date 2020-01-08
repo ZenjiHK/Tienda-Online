b63fef21e1debe841df5f6363548924b0d1cf4d1
Efelyn <58366242+Efelyn@users.noreply.github.com>
2020-01-08 15:36:30 -0600

    Update DetalleTarjetaController.java

--- src/main/java/Controller/DetalleTarjetaController.java
+++ src/main/java/Controller/DetalleTarjetaController.java
@@ -62,7 +62,7 @@ public class DetalleTarjetaController implements Serializable {
             limpiar();
             this.msj = "Detalle de tarjeta ingresado correctamente";
         } catch (Exception e) {
-            this.msj = "Error al ingresar" + e.getMessage();
+           this.msj = "El numero de tarjeta ya esta registrado, favor ingrese uno nuevo";
             e.printStackTrace();
         }
         FacesMessage mensaje = new FacesMessage(this.msj);
