96712f5918f8f533933bb0a9e014e9795f2cc98a
Juan-Torres96 <ynwa12345>
2020-01-08 09:10:32 -0600

    venta controller, detalle venta controller

--- src/main/java/Controller/DetalleVentaController.java
+++ src/main/java/Controller/DetalleVentaController.java
@@ -1,12 +1,17 @@
 package Controller;
 
 import EJB.DetalleVentaFacadeLocal;
+import EJB.ProductoFacadeLocal;
 import Entity.DetalleVenta;
+import Entity.Producto;
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 /**
@@ -15,12 +20,19 @@ import javax.inject.Named;
  */
 @Named(value = "detalleVentaController")
 @SessionScoped
-public class DetalleVentaController implements Serializable{
-    
+public class DetalleVentaController implements Serializable {
+
     @EJB
     private DetalleVentaFacadeLocal detalleVentaEJB;
     private DetalleVenta detalleVenta;
     private List<DetalleVenta> lista;
+    private List<Producto> list;
+    private String msg;
+    private double total;
+    private int contador = 0;
+    private ProductoFacadeLocal productoEJB;
+    
+    
 
     public DetalleVenta getDetalleVenta() {
         return detalleVenta;
@@ -37,41 +49,44 @@ public class DetalleVentaController implements Serializable{
     public void setLista(List<DetalleVenta> lista) {
         this.lista = lista;
     }
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         detalleVenta = new DetalleVenta();
+        list = new LinkedList<>();
+        
+
     }
-    
-    public void insertar(){
+
+    public void insertar() {
         try {
             detalleVentaEJB.create(detalleVenta);
         } catch (Exception e) {
         }
     }
-    
-    public void listar(){
+
+    public void listar() {
         try {
             lista = detalleVentaEJB.findAll();
         } catch (Exception e) {
         }
     }
-    
-    public void leerid(DetalleVenta dv){
+
+    public void leerid(DetalleVenta dv) {
         try {
             this.detalleVenta = dv;
         } catch (Exception e) {
         }
     }
-    
-    public void modificar(){
+
+    public void modificar() {
         try {
             detalleVentaEJB.edit(detalleVenta);
         } catch (Exception e) {
         }
     }
-    
-    public void eliminar(DetalleVenta dv){
+
+    public void eliminar(DetalleVenta dv) {
         try {
             this.detalleVenta = dv;
             detalleVentaEJB.remove(detalleVenta);
@@ -79,5 +94,74 @@ public class DetalleVentaController implements Serializable{
         } catch (Exception e) {
         }
     }
-    
+    public void delete(Producto pr){
+        try {
+            list.remove(pr);
+            msg = "Producto eliminado";
+        } catch (Exception e) {
+            this.msg = "Error";
+            e.printStackTrace();
+        }
+        FacesMessage msj = new FacesMessage(msg);
+        FacesContext.getCurrentInstance().addMessage(msg, msj);
+    }
+
+    public void añadir(Producto p) {
+        try {
+            list.add(p);
+            msg = "Se agrego producto al carrito";
+            Total();
+        } catch (Exception e) {
+            this.msg = "Error";
+            e.printStackTrace();
+        }
+        FacesMessage msj = new FacesMessage(msg);
+        FacesContext.getCurrentInstance().addMessage(msg, msj);
+    }
+
+    public void guardar() {
+        int tamaño = lista.size();
+        while (tamaño > 0) {
+            this.detalleVentaEJB.create(lista.get(tamaño + 1));
+
+            tamaño = tamaño + 1;
+        }
+
+    }
+
+    public List<Producto> getList() {
+        return list;
+    }
+
+    public void setList(List<Producto> list) {
+        this.list = list;
+    }
+
+    public void Total() {
+        total = 0;
+        for (int a = 0; a < list.size(); a++) {
+            total = total + list.get(a).getPrecioVenta();
+
+        }
+        contador++;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+
+ 
 }
--- target/generated-sources/annotations/Entity/DetalleVenta_.java
+++ target/generated-sources/annotations/Entity/DetalleVenta_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T10:51:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:16:39")
 @StaticMetamodel(DetalleVenta.class)
 public class DetalleVenta_ { 
 
