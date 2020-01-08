cd6453a4a828434603d1d1d3ee266286c35d4c0f
ZenjiHK <zenji503@gmail.com>
2020-01-08 14:23:39 -0600

    Detalle tarjeta

---
+++ target/generated-sources/annotations/Entity/Categoria_.java
@@ -0,0 +1,17 @@
+package Entity;
+
+import Entity.Producto;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T14:20:30")
+@StaticMetamodel(Categoria.class)
+public class Categoria_ { 
+
+    public static volatile SingularAttribute<Categoria, Integer> idCategoria;
+    public static volatile ListAttribute<Categoria, Producto> lista_productos;
+    public static volatile SingularAttribute<Categoria, String> nombreCategoria;
+
+}
---
+++ target/generated-sources/annotations/Entity/Cliente_.java
@@ -0,0 +1,26 @@
+package Entity;
+
+import Entity.DetalleTarjeta;
+import Entity.Pais;
+import Entity.User;
+import Entity.Venta;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T14:20:30")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile ListAttribute<Cliente, Venta> lista_ventas;
+    public static volatile SingularAttribute<Cliente, Integer> idCliente;
+    public static volatile SingularAttribute<Cliente, String> nombreCliente;
+    public static volatile ListAttribute<Cliente, DetalleTarjeta> lista_detalle_tarjetas;
+    public static volatile SingularAttribute<Cliente, String> apellidoCliente;
+    public static volatile SingularAttribute<Cliente, String> correo;
+    public static volatile SingularAttribute<Cliente, String> direccion;
+    public static volatile ListAttribute<Cliente, User> lista_users;
+    public static volatile SingularAttribute<Cliente, Pais> pais;
+
+}
