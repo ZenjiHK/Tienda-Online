28f08d327d7f0244ccd2b4c0104b971efd3f81b5
ZenjiHK <zenji503@gmail.com>
2020-01-08 16:50:15 -0600

    index

--- target/generated-sources/annotations/Entity/Marca_.java
+++ target/generated-sources/annotations/Entity/Marca_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T14:20:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T16:37:28")
 @StaticMetamodel(Marca.class)
 public class Marca_ { 
 
