bf0789bbc80acbbaaf91899e936c04e8d30bfc81
Gamzeescript <gamzeescript@outlook.com>
2020-01-09 16:35:02 -0600

    fdsfds
    fdsf

--- target/generated-sources/annotations/Entity/DetalleVenta_.java
+++ target/generated-sources/annotations/Entity/DetalleVenta_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T14:20:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T10:47:00")
 @StaticMetamodel(DetalleVenta.class)
 public class DetalleVenta_ { 
 
