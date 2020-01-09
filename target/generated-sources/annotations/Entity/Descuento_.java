729ce369acfdc4b8b5e66bfb590ab2483022f6c9
Alex Lemuz <lemuz101@gmail.com>
2020-01-09 15:57:10 -0600

    mantenimiento

--- target/generated-sources/annotations/Entity/Descuento_.java
+++ target/generated-sources/annotations/Entity/Descuento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T08:15:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T15:56:21")
 @StaticMetamodel(Descuento.class)
 public class Descuento_ { 
 
