909363fa40728fc9b91517d54d96f74e2df0f29e
maynor <maynornmenjivarp@gmail.com>
2020-01-13 14:42:04 -0600

    datos de perfil usuario

--- src/main/java/Controller/ValidarSesionController.java
+++ src/main/java/Controller/ValidarSesionController.java
@@ -7,6 +7,7 @@
 package Controller;
 
 import Entity.User;
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -17,13 +18,13 @@ import javax.faces.context.FacesContext;
  */
 @ManagedBean
 @SessionScoped
-public class ValidarSesionController {
-
+public class ValidarSesionController implements Serializable{
+    String redirect="";
     public void validarSesion() {
         try {
             User dato = (User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
             if (dato == null) {
-                FacesContext.getCurrentInstance().getExternalContext().redirect("ErrorSesion.xhtml");
+                FacesContext.getCurrentInstance().getExternalContext().redirect("./../PaginaPrincipal/Usuario.xhtml");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,9 +35,22 @@ public class ValidarSesionController {
 
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect("Usuario.xhtml");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("./../PaginaPrincipal/Usuario.xhtml");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public String redireccion(){
+    redirect="/User/perfilUsuario?faces-redirect=true";
+    
+    return redirect;
+    
+    }
+    public String redireccionTargetas(){
+    redirect="/User/misTarjetas?faces-redirect=true";
+    
+    return redirect;
+    
     }
 }
